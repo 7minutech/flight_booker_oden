dac5ccd806e14d0f3428cdf760d14eb99cf53d95
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-16 14:54:58 -0500

    feat: departure airport ref to flights migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_16_194802) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_16_195322) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -23,8 +23,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_16_194802) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "arrival_airport_id", null: false
+    t.integer "departure_airport_id", null: false
     t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
