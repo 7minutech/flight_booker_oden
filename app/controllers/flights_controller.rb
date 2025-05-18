b8431dd4f5db76d18d2acef58a38cf71551ec646
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-17 20:10:17 -0500

    feat: instance vars in index action for search from

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,8 @@
 class FlightsController < ApplicationController
+  def index
+    @departure_airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
+    @arrival_airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
+    @passenger_numbers = (1..4).map { |num| [ num, num ] }
+    @flight_dates = Flight.unique_ordered_dates.map { |date| [ date, date ] }
+  end
 end
