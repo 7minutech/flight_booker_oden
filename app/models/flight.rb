b3cd8219f15e4cefc40b831b90e8d5b3cb98fa49
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-17 20:07:51 -0500

    feat: methods for dates

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,16 @@ class Flight < ApplicationRecord
 
   validates :start_timestamp, presence: true
   validates :flight_duration, presence: true
+
+  scope :ordered_timestamps, -> { Flight.all.order(:start_timestamp) }
+
+  def formatted_date
+    start_timestamp.strftime("%m/%d/%Y")
+  end
+
+  def self.unique_ordered_dates
+    formatted_dates = ordered_timestamps.map { |flight| flight.formatted_date }
+    unique_formatted_dates = formatted_dates.uniq
+    unique_formatted_dates
+  end
 end
