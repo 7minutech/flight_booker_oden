f8cd607ef20ad17b8e4d9499955e7677c1995bb2
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-18 20:52:56 -0500

    feat: instance var for available flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,6 @@ class FlightsController < ApplicationController
     @arrival_airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
     @passenger_numbers = (1..4).map { |num| [ num, num ] }
     @flight_dates = Flight.unique_ordered_dates.map { |k, v| [ v, k ] }
+    @available_flights = Flight.on(params[:departure_airport_id], params[:arrival_airport_id], params[:flight_date])
   end
 end
