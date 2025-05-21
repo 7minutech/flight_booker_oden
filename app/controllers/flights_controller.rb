6f430032d09270f582f4f2a4f932ddf1e0f0f2a9
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-20 20:27:59 -0500

    fix: assign available_flights only if params are present

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,6 +4,8 @@ class FlightsController < ApplicationController
     @arrival_airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
     @passenger_numbers = (1..4).map { |num| [ num, num ] }
     @flight_dates = Flight.unique_ordered_dates.map { |k, v| [ v, k ] }
-    @available_flights = Flight.on(params[:departure_airport_id], params[:arrival_airport_id], params[:flight_date])
+    if params[:departure_airport_id].present? && params[:arrival_airport_id].present? && params[:flight_date].present?
+      @available_flights = Flight.on(params[:departure_airport_id], params[:arrival_airport_id], params[:flight_date])
+    end
   end
 end
