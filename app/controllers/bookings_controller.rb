e064d17957ea99d45768ea290c668e510c9f9547
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-20 20:50:28 -0500

    feat: booking new actions builds passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,5 +2,9 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
+    @passenger_num = params[:ticket_num].to_i
+    @passenger_num.times do
+      @booking.passengers.build
+    end
   end
 end
