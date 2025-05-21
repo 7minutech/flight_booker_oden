decbfca816a1f4b5a8fda1a9b76972cf4681587d
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-20 21:16:16 -0500

    feat: passengers_attributes setter

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,10 @@ class Booking < ApplicationRecord
   belongs_to :flight
   has_many :passengers
   accepts_nested_attributes_for :passengers
+
+  def passengers_attributes=(passengers_attributes)
+    passengers_attributes.each do |i, passengers_attributes|
+      self.passengers.build(passengers_attributes)
+    end
+  end
 end
